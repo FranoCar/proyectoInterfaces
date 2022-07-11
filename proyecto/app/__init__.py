02bf665e529a041d161bcf69fef79cce57d80eac
Franco Carrión <franico_c@hotmail.com>
2022-07-10 23:04:30 -0400

    Avance en catalogo

--- proyecto/app/__init__.py
+++ proyecto/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, redirect, url_for, request
-
+import sqlite3
 
 #----------------- Aquí hacer todos los imports y funciones y variables globales!!! -----------------
 # Luego se importan los paquetes en routes.py de forma: "from app import [paquete,función o variable en __init__.py]"
